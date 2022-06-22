31c5b090a6671a0f0199d40fb007104ab99bc742
stefanoalmeida <stefano.almeida90@gmail.com>
2022-06-22 00:11:11 -0300

    Arquivos aula 01

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+
+  console.log(data)
+
+  return (
+    <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+  )
+}
+
+export default App
