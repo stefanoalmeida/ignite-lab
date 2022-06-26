db7a2b766884255226102f3eb63e656747e85bee
stefanoalmeida <stefano.almeida90@gmail.com>
2022-06-26 19:59:11 -0300

    Aula 05 finalizada

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query MyQuery {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}`
-
-interface GetLessonsQueryResponse {
-  lessons:{
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: 'live' | 'class'
-  }[]
-}
-
-export function Sidebar(){
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+export function Sidebar() {
+  const { data } = useGetLessonsQuery()
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-1 border-gray-600">
